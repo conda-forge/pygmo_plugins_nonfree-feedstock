35a6c931ce699adaf56f4f417e754559ceb53f8a
Dario Izzo <dario.izzo@gmail.com>
2018-04-04 15:27:44 +0200

    attempt to fix osx failiure on py36

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -8,5 +8,5 @@ sleep 20;
 # Run the test suite
 python -c "import pygmo_plugins_nonfree; pygmo_plugins_nonfree.test.run_test_suite()"
 
-# Stop the cluster.
-ipcluster stop
+# Stop the cluster. (this is commented out as XCodepy36 builds failed)
+# ipcluster stop
