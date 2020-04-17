17da7bbed4128544b38c713d90c6a905bddd9b46
Dario Izzo <dario.izzo@gmail.com>
2020-04-17 11:34:27 +0200

    pinning pybind to commit

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,21 @@
 mkdir build_cpp
 cd build_cpp
+SET PPNF_BUILD_DIR=%cd%
+
+git clone https://github.com/pybind/pybind11.git
+cd pybind11
+git checkout 4f72ef846fe8453596230ac285eeaa0ce3278bb4
+mkdir build
+cd build
+cmake ^
+    -G "Ninja" ^
+    -DPYBIND11_TEST=NO ^
+    -DCMAKE_INSTALL_PREFIX=%PPNF_BUILD_DIR% ^
+    -DCMAKE_PREFIX_PATH=%PPNF_BUILD_DIR% ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    ..
+cmake --build . --target install
+cd ../..
 
 cmake ^
     -G "NMake Makefiles" ^
@@ -23,6 +39,7 @@ cmake ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DPPNF_BUILD_CPP=no ^
     -DPPNF_BUILD_PYTHON=yes ^
+    -Dpybind11_DIR=%PYAUDI_BUILD_DIR%\share\cmake\pybind11\ ^
     ..
 
 cmake --build . --config Release --target install
