e3d6a7dac628eae999fa356e9b494e08eda5a2dd
Dario Izzo <dario.izzo@gmail.com>
2026-03-05 12:22:07 +0100

    Refactor build script for C++ and Python builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,51 +1,48 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Satisfy brittle exported link interface that references an absolute .../sysroot/usr/lib/libm.so
-# Search any sysroot in the current build prefix tree and create usr/lib/libm.so if missing.
+# Always build from the extracted sources
+cd "${SRC_DIR}"  # SRCDIR is set by conda-build/rattler-build [file:77]
+
+# Workaround for brittle exported link interface:
+# some dependency exports an absolute .../sysroot/usr/lib/libm.so path. [file:77]
 for sysroot in $(find "${BUILD_PREFIX}" -path "*/sysroot" -type d 2>/dev/null || true); do
   if [[ ! -e "${sysroot}/usr/lib/libm.so" ]]; then
     mkdir -p "${sysroot}/usr/lib"
-    # Try common locations for the real SONAME
     for cand in \
       "${sysroot}/usr/lib64/libm.so.6" \
       "${sysroot}/lib64/libm.so.6" \
       "${sysroot}/usr/lib/x86_64-linux-gnu/libm.so.6" \
       "${sysroot}/usr/lib/libm.so.6"
     do
-      if [[ -e "$cand" ]]; then
-        ln -s "$cand" "${sysroot}/usr/lib/libm.so"
+      if [[ -e "${cand}" ]]; then
+        ln -s "${cand}" "${sysroot}/usr/lib/libm.so"
         break
       fi
     done
   fi
 done
 
-# ppnf (C++)
-cmake ${CMAKE_ARGS} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DPPNF_BUILD_CPP=yes \
-    -DPPNF_BUILD_TESTS=no \
-    ..
+########################################
+# C++ build
+########################################
+cmake -S . -B build_cpp ${CMAKE_ARGS} \
+  -DPPNF_BUILD_CPP=ON \
+  -DPPNF_BUILD_PYTHON=OFF \
+  -DPPNF_BUILD_TESTS=OFF \
+  -DCMAKE_BUILD_TYPE=Release
 
-make -j${CPU_COUNT} VERBOSE=1
-make install
-cd ..
+cmake --build build_cpp --parallel "${CPU_COUNT}"
+cmake --install build_cpp
 
-# ppnf (python)
-mkdir build_python
-cd build_python
+########################################
+# Python build
+########################################
+cmake -S . -B build_py ${CMAKE_ARGS} \
+  -DPPNF_BUILD_CPP=OFF \
+  -DPPNF_BUILD_PYTHON=ON \
+  -DPPNF_BUILD_TESTS=OFF \
+  -DPython3_EXECUTABLE="${PYTHON}"
 
-cmake \
-    -DPython3_EXECUTABLE=$PREFIX/bin/python \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DPPNF_BUILD_CPP=no \
-    -DPPNF_BUILD_PYTHON=yes \
-    ..
-
-make -j${CPU_COUNT} VERBOSE=1
-make install
+cmake --build build_py --parallel "${CPU_COUNT}"
+cmake --install build_py
