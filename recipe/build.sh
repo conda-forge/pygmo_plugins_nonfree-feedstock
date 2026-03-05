ee21b4539c50f4b47f8fca9688da087f50e8c341
Dario Izzo <dario.izzo@gmail.com>
2026-03-05 12:24:47 +0100

    Clean up build.sh by removing commented code
    Removed commented sections for C++ and Python build steps.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Always build from the extracted sources
-cd "${SRC_DIR}"  # SRCDIR is set by conda-build/rattler-build [file:77]
+cd "${SRC_DIR}"
 
-# Workaround for brittle exported link interface:
-# some dependency exports an absolute .../sysroot/usr/lib/libm.so path. [file:77]
 for sysroot in $(find "${BUILD_PREFIX}" -path "*/sysroot" -type d 2>/dev/null || true); do
   if [[ ! -e "${sysroot}/usr/lib/libm.so" ]]; then
     mkdir -p "${sysroot}/usr/lib"
@@ -23,9 +20,6 @@ for sysroot in $(find "${BUILD_PREFIX}" -path "*/sysroot" -type d 2>/dev/null ||
   fi
 done
 
-########################################
-# C++ build
-########################################
 cmake -S . -B build_cpp ${CMAKE_ARGS} \
   -DPPNF_BUILD_CPP=ON \
   -DPPNF_BUILD_PYTHON=OFF \
@@ -35,9 +29,6 @@ cmake -S . -B build_cpp ${CMAKE_ARGS} \
 cmake --build build_cpp --parallel "${CPU_COUNT}"
 cmake --install build_cpp
 
-########################################
-# Python build
-########################################
 cmake -S . -B build_py ${CMAKE_ARGS} \
   -DPPNF_BUILD_CPP=OFF \
   -DPPNF_BUILD_PYTHON=ON \
